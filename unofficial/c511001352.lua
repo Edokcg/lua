66e4c7f34b43a4d1aa0a7ddcd75975c2163a1b71
knight00 <perfectdicky@yahoo.com.hk>
2026-09-08 08:00:25 +0800

    Added new card script

---
+++ unofficial/c511001352.lua
@@ -0,0 +1,36 @@
+--虫媒花の園
+--Insect Garden
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(function() Duel.AdjustInstantly() end)
+	c:RegisterEffect(e1)
+	--Give control of any Level 4 or lower Insect monster you control to your opponent
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ADJUST)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetOperation(s.ctrlop)
+	c:RegisterEffect(e2)
+end
+function s.ctrlfilter(c)
+	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_INSECT) and c:IsControlerCanBeChanged() 
+end
+function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.ctrlfilter,tp,LOCATION_MZONE,0,nil)
+	local oft=Duel.GetMZoneCount(1-tp)
+	if #g==0 or oft<=0 then return end 
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+		local sg=g:FilterSelect(tp,s.ctrlfilter,1,oft,nil)
+		if #sg>0 then
+			Duel.HintSelection(sg)
+			Duel.GetControl(sg,1-tp)
+		end
+	end
+end
